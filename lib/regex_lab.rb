c4db084a36a136080fc791fad21904f3e94a72bc
Paul Ashour <paul.ashour@gmail.com>
2018-10-29 22:29:52 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  word.downcase.match(/^[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words_arr = text.split
+  words_arr.grep(/\bun|ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  words_arr = text.split
+  words_arr.grep(/^\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].*[^\w\w]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/^\W*\d{3}\W*\d{3}\W*\d{4}\W*$/)
 end
