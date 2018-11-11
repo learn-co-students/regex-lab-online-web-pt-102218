7b727876017267c6f78e2ae0f81e4994832c3320
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-11 16:10:59 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiouAEIOU]/)
+    return true 
+  end 
+  return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.split(" ").grep(/[uU]n[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*[\/.?!]\z/)
+    return true 
+  end 
+  return false
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{10}|[(]\d{3}[)]\d{3}[-]\d{4}\z|(\d{3}\s){2}\d{4}|[(]\d{3}[)]\d{7}/)
+    return true
+  end 
+  return false
 end
